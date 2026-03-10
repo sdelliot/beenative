a5e0c67122e3734aead1194f7ac31abdc56228f1
Steven Elliott <6461548+sdelliot@users.noreply.github.com>
2026-03-09 20:55:26 -0400

    Running chores

--- beenative/models/__init__.py
+++ beenative/models/__init__.py
@@ -4,7 +4,4 @@ from pathlib import Path
 current_dir = Path(__file__).parent
 
 # Use .glob() and filter using Path properties
-__all__ = [
-    f.stem for f in current_dir.glob("*.py")
-    if f.is_file() and f.name != "__init__.py"
-]
+__all__ = [f.stem for f in current_dir.glob("*.py") if f.is_file() and f.name != "__init__.py"]
